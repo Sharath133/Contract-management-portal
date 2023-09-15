e072aa1bf6d2bef9e4790e9321e01edbd3bb1bec
Sharath <87656483+Sharath133@users.noreply.github.com>
2023-09-15 21:52:42 +0530

    Delete Contract managment Portal directory

--- Contract managment Portal/DEP_backend-main/middleware/index.js
+++
@@ -1,9 +0,0 @@
-const authJwt = require('../middleware/authJwt');
-const verifySignUp = require('../middleware/verifySignUp');
-const adminControls= require('../middleware/adminControls');
-
-module.exports = {
-    authJwt,
-    verifySignUp,
-    adminControls,
-}
--- Contract managment Portal/DEP_frontend-main/src/pages/admin/admin_home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function admin_home() {
-  return (
-    <div>admin_home</div>
-  )
-}
-
-export default admin_home
